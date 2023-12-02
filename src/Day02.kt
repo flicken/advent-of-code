98c3e3e183115f22823529a74aba49dd38278b75
Brian Roberts <flicken-github@flicken.net>
2023-12-02 19:34:47 +0100

    Working solution for day 2

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,16 +1,48 @@
+import kotlin.math.max
+
 fun main() {
+    fun showingToMap(showing: String): Map<String, Int> {
+        return showing.split(", ").associate {
+            val (countS, color) = it.split(" ");
+            color to countS.toInt();
+        };
+    }
+
+    val part1Max = showingToMap("12 red, 13 green, 14 blue");
+
     fun part1(input: List<String>): Int {
-        input.
-        return input.size
+        return input.sumOf { line ->
+            val parts = line.split("; ", ": ");
+            val game = parts[0].split(" ")[1].toInt();
+
+            val allPass = parts.drop(1).map{showingToMap(it)}.all { showing ->
+                showing.all { (color, count) ->
+                    part1Max.getValue(color) >= count
+                }
+            };
+
+            if (allPass) game else 0
+        };
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.sumOf { line ->
+            val parts = line.split("; ", ": ")
+
+            val maximums = parts.drop(1).map{showingToMap(it)}.reduce { maxNeeded, cur ->
+                part1Max.keys.map { color ->
+                    color to max(maxNeeded.getOrDefault(color, 0), cur.getOrDefault(color, 0))
+                }.toMap()
+            };
+
+            maximums.values.reduce{a, b -> a * b}
+        };
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day02_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 8)
+    check(part2(testInput) == 2286)
 
     val input = readInput("Day02")
     part1(input).println()
