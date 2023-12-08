a366c8023d0bb09d6d563fc7eb622a18f48c64ed
Brian Roberts <flicken-github@flicken.net>
2023-12-08 12:40:19 +0100

    Day 8: Add part2
    Needed another algorithm.  This is not a general solution, but tailored to the input graph
    which starts a cycle.

--- src/Day08.kt
+++ src/Day08.kt
@@ -1,6 +1,23 @@
-data class Maze(val paths: Map<String, Pair<String, String>>, val moves: CharArray)
+import kotlin.math.max
+import kotlin.math.min
+
+fun lcm(a: Long, b: Long): Long {
+    val larger = max(a, b)
+    val smaller = min(a, b)
+    var lcm = larger
+    while (lcm % smaller != 0L) {
+        lcm += larger
+    }
+    return lcm
+}
+
+fun List<Long>.lcm(): Long {
+    return this.reduce { a, b -> lcm(a, b) }
+}
 
 fun main() {
+    data class Maze(val paths: Map<String, Pair<String, String>>, val moves: CharArray, val names: Array<String>)
+
     val day = "Day08"
 
     fun nextMove(location: String, step: Long, maze: Maze): String {
@@ -16,6 +33,7 @@ fun main() {
         from: String,
         to: String,
         maze: Maze,
+        stepStart: Long = 0
     ): Long {
         tailrec fun iter(location: String, step: Long): Long {
             if (location == to) {
@@ -25,9 +43,8 @@ fun main() {
             }
         }
 
-        return iter(from, 0);
+        return iter(from, stepStart);
     }
-
 
     fun parseMovesAndPaths(input: List<String>): Maze {
         val moves = input.first().toCharArray()
@@ -35,9 +52,9 @@ fun main() {
         val paths = input.drop(2).map {
             val (name, left, right) = "[^A-Z0-9]+".toRegex().split(it)
             name to Pair(left, right)
-        }.toMap()
+        }
 
-        return Maze(paths, moves)
+        return Maze(paths.toMap(), moves, paths.map { it.first }.toTypedArray())
     }
 
     fun part1(input: List<String>): Long {
@@ -46,8 +63,49 @@ fun main() {
         return countMoves("AAA", "ZZZ", maze)
     }
 
+
+    fun findCycles(from: String, to: String, maze: Maze): List<Long> {
+        val starts = maze.names.filter { it.endsWith(from) }
+        val ends = maze.names.filter { it.endsWith(to) }
+
+        val foundStarts = mutableMapOf<String, Pair<String, Long>>()
+        tailrec fun iterStarts(locations: List<String>, step: Long) {
+            locations.forEachIndexed<String> { i, e ->
+                if (ends.contains(e)) {
+                    foundStarts.put(starts[i], Pair(e, step));
+                }
+            }
+
+            if (foundStarts.size == starts.size) {
+                return;
+            } else {
+                return iterStarts(locations.map { nextMove(it, step, maze) }, step + 1)
+            }
+        }
+        iterStarts(starts, 0)
+
+        return foundStarts.values.map<Pair<String, Long>, Long> { (end, originalStep) ->
+            val foundMoveEnds = mutableMapOf<Pair<String, Long>, Long>();
+            tailrec fun iterEnds(location: String, step: Long): Long {
+                if (ends.contains(location)) {
+                    val move = Pair(location, step % maze.moves.size)
+                    if (foundMoveEnds.contains(move)) {
+                        return step - foundMoveEnds.getOrDefault(move, Long.MAX_VALUE)
+                    } else {
+                        foundMoveEnds.put(move, step)
+                    }
+                }
+                return iterEnds(nextMove(location, step, maze), step + 1)
+            }
+
+            iterEnds(nextMove(end, originalStep, maze), originalStep + 1)
+        }
+    }
+
     fun part2(input: List<String>): Long {
-        return -1
+        val maze = parseMovesAndPaths(input)
+
+        return findCycles("A", "Z", maze).lcm()
     }
 
     val testInput = readInput("${day}_test")
