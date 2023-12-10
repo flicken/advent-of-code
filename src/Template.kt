a7b6e3a02bf5c26f526edc9eb8d8d421b7c8af26
Brian Roberts <flicken-github@flicken.net>
2023-12-10 20:11:49 +0100

    Make outputs Long, because many problems need a long

--- src/Template.kt
+++ src/Template.kt
@@ -1,20 +1,20 @@
 fun main() {
     val day = "Day__DAY__"
 
-    fun part1(input: List<String>): Int {
+    fun part1(input: List<String>): Long {
         return -1
     }
 
-    fun part2(input: List<String>): Int {
+    fun part2(input: List<String>): Long {
         return -1
     }
 
     val testInput = readInput("${day}_test")
-    part1(testInput).assertEqual(-42)
+    part1(testInput).assertEqual(-42L)
 
     val input = readInput(day)
     timeAndPrint { part1(input) }
 
-    part2(testInput).assertEqual(-43)
+    part2(testInput).assertEqual(-43L)
     timeAndPrint { part2(input) }
 }
