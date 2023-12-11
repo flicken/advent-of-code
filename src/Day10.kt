8347a277381617ea807805316de52ce6a7ff4b13
Brian Roberts <flicken-github@flicken.net>
2023-12-11 14:58:48 +0100

    Day 10: Use raycasting inside-outside algorithm
    Had an aborted attempt at a flood-fill algorithm, but this was simpler.
    
    https://iq.opengenus.org/inside-outside-test/

--- src/Day10.kt
+++ src/Day10.kt
@@ -1,73 +1,142 @@
-data class Location(val line: Int, val col: Int)
+import Direction.*
+
+data class Location(val row: Int, val col: Int) {
+    fun go(direction: Direction) = when (direction) {
+        Up -> this.copy(row = this.row - 1)
+        Down -> this.copy(row = this.row + 1)
+        Left -> this.copy(col = this.col - 1)
+        Right -> this.copy(col = this.col + 1)
+    }
+}
+
+enum class Direction {
+    Up, Down, Left, Right
+}
+
+data class Move(val location: Location, val direction: Direction) {
+    fun move(d: Direction) = this.copy(this.location.go(d), d)
+}
 
 fun main() {
     val day = "Day10"
 
+    fun printPath(input: List<String>, path: Set<Location>, outside: Set<Location>? = null) {
+        println(String(CharArray(input[0].length + 2, { 'x' })))
+        input.forEachIndexed { row, line ->
+            line.mapIndexed { col, c ->
+                val location = Location(row, col)
+                if (path.contains(location)) {
+                    c
+                } else {
+                    if (outside == null || outside.contains(location)) " " else '.'
+                }
+
+            }.joinToString("").let { "${row % 10}${it}${row % 10}" }.println()
+        }
+        println(String(CharArray(input[0].length + 2, { i -> (i % 10).toString().get(0) })))
+    }
 
     fun findStart(maze: List<String>): Location {
-        val line = maze.indexOfFirst { it.contains('S') }
-        val col = maze[line].indexOf('S')
+        val row = maze.indexOfFirst { it.contains('S') }
+        val col = maze[row].indexOf('S')
 
-        return Location(line, col);
+        return Location(row, col)
     }
 
-    val m = "-L|F7\n" +
-            "7S-7|\n" +
-            "L|7||\n" +
-            "-L-J|\n" +
-            "L|-JF\n"
+    operator fun List<String>.get(location: Location): Char = this[location.row][location.col]
 
-    fun findConnectingLocations(maze: List<String>, location: Location): List<Location> {
-
-        return listOf();
-    }
-
-    fun Location.up() = this.copy(line = this.line - 1)
-    fun Location.down() = this.copy(line = this.line + 1)
-    fun Location.left() = this.copy(col = this.col - 1)
-    fun Location.right() = this.copy(col = this.col + 1)
-
-    fun findNext(maze: List<String>, location: Location, prev: Location): Location {
-        return when (maze[location.line][location.col]) {
-            'S' -> location.right() // special case for puzzle
-            'L' -> if (location.up() == prev) location.right() else location.up()
-            '7' -> if (location.left() == prev) location.down() else location.left()
-            'J' -> if (location.left() == prev) location.up() else location.left()
-            '-' -> if (location.left() == prev) location.right() else location.left()
-            '|' -> if (location.up() == prev) location.down() else location.up()
-            'F' -> if (location.down() == prev) location.right() else location.down()
-            else -> throw Exception("Cannot find next location")
+    fun findNextMove(maze: List<String>, move: Move, startSymbol: Char): Move {
+        val cameVia = move.direction
+        return when (maze[move.location]) {
+            'S' -> if (startSymbol == '7') move.move(Down) else move.move(Right) // handles all puzzle inputs
+            'L' -> if (cameVia == Down) move.move(Right) else move.move(Up)
+            '7' -> if (cameVia == Right) move.move(Down) else move.move(Left)
+            'J' -> if (cameVia == Right) move.move(Up) else move.move(Left)
+            '-' -> if (cameVia == Right) move.move(Right) else move.move(Left)
+            '|' -> if (cameVia == Down) move.move(Down) else move.move(Up)
+            'F' -> if (cameVia == Up) move.move(Right) else move.move(Down)
+            else -> throw Exception("Cannot find next location at ${move.location}: ${maze[move.location]}")
         }
     }
 
-    fun part1(input: List<String>): Long {
-        val maze = input
-        val start = findStart(maze);
-        var location = start;
-        var prev = start;
-        var count = 0;
+    fun findMoves(maze: List<String>, startSymbol: Char = 'F'): List<Move> {
+        val start = findStart(maze)
+        val moves = mutableListOf(Move(start, Right))
 
-        do {
-            count += 1
-            val next = findNext(maze, location, prev)
-            prev = location
-            location = next
-        } while (location != start)
-
-        return (count / 2).toLong()
+        while (true) {
+            val next = findNextMove(maze, moves.last(), startSymbol)
+            if (next.location == start) {
+                break
+            }
+            moves.add(next)
+        }
+        return moves
     }
 
-    fun part2(input: List<String>): Long {
+    fun part1(input: List<String>): Long {
+        val path = findMoves(input)
+        printPath(input, path.map { it.location }.toSet())
 
-        return -1
+        return (path.size / 2).toLong()
+    }
+
+    fun part2(maze: List<String>, startSymbol: Char = 'F'): Long {
+        val pathSet = findMoves(maze, startSymbol).map { it.location }.toSet()
+
+        val insides = mutableListOf<Location>()
+        val outsides = mutableSetOf<Location>()
+        maze.forEachIndexed { row, line ->
+            var crossings = 0
+            line.forEachIndexed { col, c ->
+                val l = Location(row, col)
+                if (pathSet.contains(l)) {
+                    if ("|".contains(maze[l])) {
+                        crossings += 2
+                    }
+                    if ("LFJ7S".contains(maze[l])) {
+                        crossings += 1
+                    }
+                } else {
+                    // Use raycasting inside outside test
+                    // See https://iq.opengenus.org/inside-outside-test/
+                    val segments = line.substring(0, col)
+                        .mapIndexed<Any> { cl, it ->
+                            val el = Location(row, cl)
+                            if (outsides.contains(el) || insides.contains(el)) " " else it
+                        }
+                        .joinToString("")
+                        .replace("S", startSymbol.toString())
+                        .replace("[-. ]".toRegex(), "").replace("FJ|L7".toRegex(), "|")
+                        .replace("F7|LJ".toRegex(), "||")
+                    if (segments.length % 2 == 0) {
+                        outsides.add(l)
+                    } else {
+                        insides.add(l)
+                    }
+                }
+            }
+        }
+
+        printPath(maze, findMoves(maze).map { it.location }.toSet(), outsides)
+        return insides.size.toLong()
     }
 
     val testInput = readInput("${day}_test")
     part1(testInput).assertEqual(4L)
 
+    println("Starting part 1")
     val input = readInput(day)
     timeAndPrint { part1(input) }
 
-    part2(testInput).assertEqual(-43)
-    timeAndPrint { part2(input) }
+    println("Part 2 test")
+    part2(testInput).assertEqual(1L)
+    println("Part 2 2")
+    part2(readInput("Day10_test2")).assertEqual(4L)
+    println("Part 2 2b")
+    part2(readInput("Day10_test2b")).assertEqual(4L)
+    println("Part 2 3")
+    part2(readInput("Day10_test3")).assertEqual(8L)
+    println("Part 2 4")
+    part2(readInput("Day10_test4"), '7').assertEqual(10L)
+    timeAndPrint { part2(input, 'L') }
 }
