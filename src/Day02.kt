28bea0fd613db3a03e582b7366c2f09e8ec317a3
Brian Roberts <flicken-github@flicken.net>
2024-12-03 06:37:56 +0100

    Clean up Day2

--- src/Day02.kt
+++ src/Day02.kt
@@ -6,23 +6,10 @@ fun main() {
         return (diffs.all { it == 1 || it == 2 || it == 3 }) || diffs.all { it == -1 || it == -2 || it == -3 }
     }
 
-    fun <T> Iterable<T>.removeIt(i: Int): Iterable<T> {
-        val l = this.toMutableList()
-        l.removeAt(i)
-        return l
-    }
-
-    fun <T> Iterable<T>.anyIndex(f: (Int) -> Boolean): Boolean {
-        return this.withIndex().any { f(it.index) }
-    }
-
-    fun Iterable<Int>.isSafeDroppingOne(): Boolean {
-        return this.isSafe() || this.anyIndex{this.removeIt(it).isSafe()}
-    }
-
-    fun String.toReport(): List<Int> {
-        return this.split(" ").map{it.toInt()}
-    }
+    fun <T> Iterable<T>.removeIt(i: Int) = this.toMutableList().tap { it.removeAt(i) }
+    fun <T> Iterable<T>.anyIndex(f: (Int) -> Boolean)= this.withIndex().any { f(it.index) }
+    fun Iterable<Int>.isSafeDroppingOne()=  this.isSafe() || this.anyIndex{this.removeIt(it).isSafe()}
+    fun String.toReport() = this.split(" ").map{it.toInt()}
 
     fun part1(input: List<String>): Long {
         return input.map{it.toReport()}.count{it.isSafe()}.toLong()
