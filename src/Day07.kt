301b0f7cc3691d778671f1d336af7eda65da8c45
Brian Roberts <flicken-github@flicken.net>
2024-12-07 16:25:39 +0100

    Day 7 improvements

--- src/Day07.kt
+++ src/Day07.kt
@@ -1,28 +1,45 @@
-import java.math.BigInteger
-
 enum class Op {
     Add, Multiply, Concatenate,
 }
 
+fun makeWayForDucklings(n: Long): Long {
+    return if (n < 10) {
+        10L
+    } else if (n < 100) {
+        100L
+    } else if (n < 1000) {
+        1000L
+    } else {
+        throw Exception("Too big!")
+    }
+}
+
+fun concat(current: Long, next: Long): Long =
+    current * makeWayForDucklings(next) + next
+
 fun main() {
     val day = "Day07"
 
-    data class Equation(val testVal: BigInteger, val numbers: List<BigInteger>, val result: BigInteger) {
+    data class Equation(val testVal: Long, val numbers: List<Long>, val result: Long) {
         fun test(op: Op): Equation =
             when (op) {
                 Op.Add -> Equation(testVal, numbers.drop(1), result + numbers[0])
                 Op.Multiply -> Equation(testVal, numbers.drop(1), result * numbers[0])
-                Op.Concatenate -> Equation(testVal, numbers.drop(1), "${result}${numbers[0]}".toBigInteger())
+                Op.Concatenate -> Equation(testVal, numbers.drop(1), concat(result, numbers[0]))
             }
     }
 
     fun String.parse(): Equation {
         val (testValS, rest) = this.split(": ")
-        val numbers = rest.split(" ").map { it.toBigInteger() }
-        return Equation(testValS.toBigInteger(), numbers.drop(1), numbers[0])
+        val numbers = rest.split(" ").map { it.toLong() }
+        return Equation(testValS.toLong(), numbers.drop(1), numbers[0])
     }
 
     fun isValid(e: Equation, ops: List<Op>): Boolean {
+        if (e.result > e.testVal) {
+            return false
+        }
+
         if (e.numbers.isEmpty()) {
             return e.testVal == e.result
         }
