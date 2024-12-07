85233aed15c9bac5abd5c22ad5df480f92a54974
Brian Roberts <flicken-github@flicken.net>
2024-12-07 16:58:11 +0100

    Day 7 - rewrite to work backwards

--- src/Day07.kt
+++ src/Day07.kt
@@ -1,7 +1,3 @@
-enum class Op {
-    Add, Multiply, Concatenate,
-}
-
 fun makeWayForDucklings(n: Long): Long {
     return if (n < 10) {
         10L
@@ -14,55 +10,50 @@ fun makeWayForDucklings(n: Long): Long {
     }
 }
 
-fun concat(current: Long, next: Long): Long =
-    current * makeWayForDucklings(next) + next
-
 fun main() {
     val day = "Day07"
 
-    data class Equation(val testVal: Long, val numbers: List<Long>, val result: Long) {
-        fun test(op: Op): Equation =
-            when (op) {
-                Op.Add -> Equation(testVal, numbers.drop(1), result + numbers[0])
-                Op.Multiply -> Equation(testVal, numbers.drop(1), result * numbers[0])
-                Op.Concatenate -> Equation(testVal, numbers.drop(1), concat(result, numbers[0]))
-            }
-    }
+    data class Equation(val testVal: Long, val numbers: List<Long>)
 
     fun String.parse(): Equation {
         val (testValS, rest) = this.split(": ")
         val numbers = rest.split(" ").map { it.toLong() }
-        return Equation(testValS.toLong(), numbers.drop(1), numbers[0])
+        return Equation(testValS.toLong(), numbers)
     }
 
-    fun isValid(e: Equation, ops: List<Op>): Boolean {
-        if (e.result > e.testVal) {
-            return false
+    fun isValid(lhs: Long, reverseNumbers: List<Long>): Boolean {
+        val next = reverseNumbers.first()
+
+        if (reverseNumbers.size == 1) {
+            return next == lhs
         }
 
-        if (e.numbers.isEmpty()) {
-            return e.testVal == e.result
+        return ((lhs % next) == 0L &&  isValid(lhs / next, reverseNumbers.drop(1))) ||
+                ((lhs > next) && isValid(lhs - next, reverseNumbers.drop(1)))
+    }
+
+    fun isValidPart2(lhs: Long, reverseNumbers: List<Long>): Boolean {
+        val next = reverseNumbers.first()
+
+        if (reverseNumbers.size == 1) {
+            return next == lhs
         }
 
-        return ops.any {
-            isValid(e.test(it), ops)
-        }
+        return (lhs.toString().endsWith(next.toString()) && isValidPart2((lhs - next) / makeWayForDucklings(next), reverseNumbers.drop(1))) ||
+                ((lhs % next) == 0L && isValidPart2(lhs / next, reverseNumbers.drop(1))) ||
+                (lhs > next && isValidPart2(lhs - next, reverseNumbers.drop(1)))
     }
 
     fun part1(input: List<String>): Long {
         val equations = input.map{it.parse()}
 
-        return equations.filter{isValid(it, listOf(Op.Add, Op.Multiply))}
-            .map{it.testVal}
-            .map{it.toLong()}.sum()
+        return equations.filter{isValid(it.testVal, it.numbers.asReversed())}.map{it.testVal}.sum()
     }
 
     fun part2(input: List<String>): Long {
         val equations = input.map{it.parse()}
 
-        return equations.filter{isValid(it, listOf(Op.Add, Op.Multiply, Op.Concatenate))}
-            .map{it.testVal}
-            .map{it.toLong()}.sum()
+        return equations.filter{isValidPart2(it.testVal, it.numbers.asReversed())}.map{it.testVal}.sum()
     }
 
     val testInput = readInput("${day}_test")
