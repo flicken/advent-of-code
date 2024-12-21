0b4f866b5f20f388c025722f505aef7f473ae722
Brian Roberts <flicken-github@flicken.net>
2024-12-21 18:58:28 +0100

    Day 21 - Part 2 attempt 3
    Fixed bug in countKeypresses that skipped the cache

--- src/Day21.kt
+++ src/Day21.kt
@@ -1,4 +1,5 @@
 import Direction.*
+import java.math.BigInteger
 
 private fun <A, B>Pair<A, B>.swap(): Pair<B, A> = this.second to this.first
 
@@ -40,13 +41,6 @@ fun main() {
         distanceBetweenDirections[entry.first.swap()] = entry.second
     }
     Direction.entries.forEach{distanceBetweenDirections[it to it] = 0}
-
-    val directionToActivate = mapOf(
-        Up to 1,
-        Right to 1,
-        Down to 2,
-        Left to 3,
-    )
 
     val expansion = mapOf(
         "<<" to "",
@@ -121,21 +115,24 @@ fun main() {
 
     val numericKeypad = listOf("789", "456", "123", "#0A")
 
-    val counts = mutableMapOf<Pair<String, Int>, Long>()
+    val counts = mutableMapOf<Pair<String, Int>, BigInteger>()
 
     (expansion.entries).forEach {
-        counts[it.key to 1] = it.value.length.toLong() + 1
+        counts[it.key to 1] = (it.value.length + 1).toBigInteger()
     }
 
-    fun String.countKeypresses(robotCount: Int): Long =
+    fun String.countKeypresses(robotCount: Int): BigInteger =
         counts.getOrPut(this to robotCount, {
-            return ("A" + this.expand()).windowed(2, 1).sumOf {
+             val expanded = "A" + this.expand()
+            val result = expanded.windowed(2, 1).sumOf {
                 it.countKeypresses(robotCount - 1)
             }
+            println("'${this}' ${robotCount} = ${result}: ${expanded}")
+            result
         })
 
 
-    fun List<String>.countKeypresses(robotCount: Int): Long {
+    fun List<String>.countKeypresses(robotCount: Int): BigInteger {
         return this.sumOf { codeS ->
             println("${codeS} counting ${robotCount}")
             val paths = numericKeypad.shortestPaths(codeS, Location(3, 2))
@@ -145,24 +142,28 @@ fun main() {
                 //                numericPresses.expand().p()
                 //                numericPresses.p()
                 //                codeS.p()
-                val length = numericPresses.countKeypresses(robotCount)
+                val length = ("A" + numericPresses.expand()).windowed(2, 1).sumOf {
+                    it.countKeypresses(robotCount - 1)
+                }
                 val code = codeS.dropLast(1).toLong()
                 println("${codeS} = ${length} * $code")
                 code to length
-            }.minOf { it.first * it.second }
+            }.map { it.second.times(it.first.toBigInteger()) }.min()
         }
     }
 
-    fun part1(input: List<String>): Long = input.countKeypresses(2)
-    fun part2(input: List<String>): Long = input.countKeypresses(25)
+    fun part1(input: List<String>): BigInteger = input.countKeypresses(2)
+    fun part2(input: List<String>): BigInteger = input.countKeypresses(25)
 
     val testInput = readInput("${day}_test")
     val input = readInput(day)
 
-    part1(testInput).assertEqual(126384L)
-    timeAndPrint { part1(input) }.also{check(it > 207912, {  "Must be larger than 207912"}) }
+    part1(testInput).assertEqual(BigInteger.valueOf(126384L))
+    timeAndPrint { part1(input) }.also{check(it > BigInteger.valueOf(207912), {  "Must be larger than 207912"}) }
 
     println("Part 2")
 //    timeAndPrint {  part2(testInput) }
     timeAndPrint { part2(input) }
+        .also{check(it < BigInteger("480122988291409916821"), {"${it} must be smaller than 480122988291409916821"})}
+        .also{check(it < BigInteger("102910099284984466724"), {"${it} must be smaller than 102910099284984466724"})}
 }
